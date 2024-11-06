86f32fb29864e74fc4a33c81bdb8a029566683d8
davidLiu <xifu_liu@hotmail.com>
2024-11-06 17:28:18 -0500

    initial structure

---
+++ gulimall-ware/src/test/java/com/rmd/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmd.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
